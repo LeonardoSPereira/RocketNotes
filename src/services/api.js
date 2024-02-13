d51bde8187a9233f1f8e6398774835b855e99a82
Leonardo Pereira <leonardo_human@hotmail.com>
2024-02-13 14:02:19 -0300

    Update API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
 
     //prod URL
-    baseURL: "https://rocketnotes-api-ywf4.onrender.com",
+    baseURL: "https://rocketnotes-api-3yrk.onrender.com",
 
     //dev URL
     //baseURL: "http://localhost:3333",
